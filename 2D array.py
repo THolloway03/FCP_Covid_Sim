4b7cd15b8c7f557d83162878cf9b80546010dd6f
THolloway03 <101190402+THolloway03@users.noreply.github.com>
2022-04-26 13:55:35 +0100

    Add files via upload
    2D array to be used as grid

---
+++ 2D array.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 18 13:26:50 2022
+
+@author: taiho
+"""
+
+
+import math
+import random
+    
+# used for testing
+def doesInfect():
+    if random.randrange(1,10) == 1 :
+        return 1
+    else: 
+        return 0
+    
+
+    
+# Used to find size that user wants the graph to be and allows it to be able to run a full number since its 
+# a 2D matrix if we ask for a population it will be the number they want^2 so to allow this to work we need to 
+# sqrt the number they ask for but that could end up with not full array sizes so we truncate it to give us a size we can use 
+
+hexMatrix_size = input('Please enter how large you wish for the sample size to be')
+hexMatrix_size = int(hexMatrix_size)
+hexMatrix_size = hexMatrix_size**(1/2)
+hexMatrix_size = math.trunc(hexMatrix_size)
+
+
+print('Your matrix will be of this size to allow it to function:')
+print(hexMatrix_size)
+
+# our goal is to have an array that allows each cell to interact with another cell (6 cells) 
+
+
+hexMatrix = [[0 for x in range(hexMatrix_size)] for y in range(hexMatrix_size)]    
+# creates the 2D array of the set size
+
+complete_Array = ' '
+Infected = 0
+hexMatrix[3][3] = 1 #TEST
+count = 0
+
+# Double for loop to run through each matrix
+for y in range(hexMatrix_size):
+    for x in range(hexMatrix_size):
+        
+        count = count+1
+        
+        if hexMatrix[x][y] == 1: #checks if infected 
+        
+            if y+1 != hexMatrix_size:
+                Infected = doesInfect()
+                hexMatrix[x][y+1] = Infected
+                Infected = 0
+            
+            
+            if y-1 > 0:
+                Infected = doesInfect()
+                hexMatrix[x][y-1] = Infected
+                Infected = 0
+
+            
+            if x-1 > 0:
+                Infected = doesInfect()            
+                hexMatrix[x-1][y] = Infected
+                Infected = 0
+
+            
+            if x-1 > 0 and y-1 > 0:
+                Infected = doesInfect()
+                hexMatrix[x-1][y-1] = Infected
+                Infected = 0
+
+            
+            if x+1 != hexMatrix_size:  
+                Infected = doesInfect()            
+                hexMatrix[x+1][y] = Infected
+                Infected = 0
+            
+            if x+1 < hexMatrix_size and y-1 > 0:
+                Infected = doesInfect()
+                hexMatrix[x+1][y-1] = Infected
+                Infected = 0
+            
+        
+        #Used to display to user for testing purposes (not complete)
+        complete_Array = (complete_Array + str(hexMatrix[x][y])) 
+        if count == hexMatrix_size-1: 
+            complete_Array = complete_Array + '\n' #used to add the entire array to a displayable string
+            if y % 2 == 1:
+                complete_Array = complete_Array + ' '
+    count = 0
+            
+print(complete_Array)
+        
